841f10d676b907be949c642afbc1014ca22290cb
Vidhur <lab.nirvana@gmail.com>
2016-07-17 17:54:11 -0700

    Added swipe to refresh functionality

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 **NeatFlickster** shows the latest movies currently playing in theaters. The app utilizes the Movie Database API to display images and basic information about these movies to the user.
 
-Time spent: **4** hours spent in total
+Time spent: **9** hours spent in total
 
 ## User Stories
 
@@ -16,7 +16,7 @@ The following **required** functionality is completed:
 
 The following **optional** features are NOT yet implemented:
 
-* [ ] User can **pull-to-refresh** popular stream to get the latest movies.
+* [x] User can **pull-to-refresh** popular stream to get the latest movies.
 * [x] Display a nice default [placeholder graphic](http://guides.codepath.com/android/Displaying-Images-with-the-Picasso-Library#configuring-picasso) for each image during loading.
 * [ ] Improved the user interface through styling and coloring.
 
--- app/src/main/java/com/task/vidhurvoora/neatflickster/MovieActivity.java
+++ app/src/main/java/com/task/vidhurvoora/neatflickster/MovieActivity.java
@@ -1,6 +1,7 @@
 package com.task.vidhurvoora.neatflickster;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.AbsListView;
@@ -20,6 +21,8 @@ public class MovieActivity extends AppCompatActivity {
     ArrayList<Movie> movieList;
     int page = 0;
 
+    private SwipeRefreshLayout movieContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +33,37 @@ public class MovieActivity extends AppCompatActivity {
 
         movieAdapter = new MovieAdapter(this,movieList);
         lvMovies.setAdapter(movieAdapter);
+        movieContainer =  (SwipeRefreshLayout) findViewById(R.id.movieContainer);
 
+        setupRefreshListener();
+        setupScrollListener();
+
+        //load movies
         loadMovies();
+    }
 
+    //refresh listener
+    private void setupRefreshListener()
+    {
+        movieContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //set the page = 0
+                //clear the movieList ?
+                page = 0;
+                movieList.clear();
+                loadMovies();
+            }
+        });
+        movieContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    //scroll listener
+    private void setupScrollListener()
+    {
         lvMovies.setOnScrollListener(new AbsListView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(AbsListView absListView, int i) {
@@ -56,6 +87,7 @@ public class MovieActivity extends AppCompatActivity {
         });
     }
 
+
     private void loadMovies()
     {
         MovieManager.getsInstance().fetchNowPlayingMovies(++page,new MovieResponseCompletionHandler() {
@@ -64,6 +96,7 @@ public class MovieActivity extends AppCompatActivity {
                 if (isSuccess) {
                     movieList.addAll(movies);
                     movieAdapter.notifyDataSetChanged();
+                    movieContainer.setRefreshing(false);
                 }
             }
         });
